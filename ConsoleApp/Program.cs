f3a402e24c1e3461169ba5137e961b4d592a2fa8
kapsarma <kapsarma@USHYDKAPSA5>
2023-01-19 15:29:54 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using OOPAdventure;
+
+Console.WriteLine("Hello What is your name?");
+
+var name = Console.ReadLine();
+
+if (name == String.Empty || name == null) {
+    name = "no name";
+}
+
+var player = new Player(name);
+
+Console.WriteLine($"Welcome {player.Name} to c# oop adventure");
